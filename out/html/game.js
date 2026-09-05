9297daf437cfe49f766b51fb85928911b66883d2
Alberto421 <59416987+Alberto421@users.noreply.github.com>
2026-09-05 17:09:56 +0200

    Remove updateSidebarRight functionality
    Removed updateSidebarRight function and related calls.

--- out/html/game.js
+++ out/html/game.js
@@ -189,14 +189,6 @@
       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
   };
 
-    window.updateSidebarRight = function() {
-    $('#qualities_right').empty();
-    var scene = dendryUI.game.scenes[window.statusTabRight];
-    dendryUI.dendryEngine._runActions(scene.onArrival);
-    var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
-    $('#qualities_right').append(dendryUI.contentToHTML.convert(displayContent));
-};
-  
   window.changeTab = function(newTab, tabId) {
       if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
           window.alert('Polls are not available in historical mode.');
@@ -208,18 +200,12 @@
         tabButtons[i].className = tabButtons[i].className.replace(' active', '');
       }
       tabButton.className += ' active';
-      if (isRight) {
-        window.statusTabRight = newTab;
-        window.updateSidebarRight();
-        } else {
-          window.statusTab = newTab;
-          window.updateSidebar();
-    }
+      window.statusTab = newTab;
+      window.updateSidebar();
   };
 
   window.onDisplayContent = function() {
       window.updateSidebar();
-      window.updateSidebarRight();
   };
 
   /*
@@ -256,7 +242,6 @@
 
   window.justLoaded = true;
   window.statusTab = "status";
-  window.statusTabRight = "status_right";
   window.dendryModifyUI = main;
   console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
 
@@ -266,10 +251,6 @@
         document.body.classList.add('dark-mode');
     }
     window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
-    window.statusTab = "status";
-    window.updateSidebar();
-    window.statusTabRight = "status_right";
-    window.updateSidebarRight();
   };
 
 }());
