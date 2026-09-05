06ad8a1d6c0ab88ff86cbed4718111612ebafbbf
Alberto421 <59416987+Alberto421@users.noreply.github.com>
2026-09-05 17:46:10 +0200

    Add updateSidebarRight function for right sidebar

--- out/html/game.js
+++ out/html/game.js
@@ -189,6 +189,14 @@
       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
   };
 
+    window.updateSidebarRight = function() {
+    $('#qualities_right').empty();
+    var scene = dendryUI.game.scenes[window.statusTabRight];
+    dendryUI.dendryEngine._runActions(scene.onArrival);
+    var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+    $('#qualities_right').append(dendryUI.contentToHTML.convert(displayContent));
+};
+  
   window.changeTab = function(newTab, tabId) {
       if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
           window.alert('Polls are not available in historical mode.');
@@ -200,8 +208,13 @@
         tabButtons[i].className = tabButtons[i].className.replace(' active', '');
       }
       tabButton.className += ' active';
-      window.statusTab = newTab;
-      window.updateSidebar();
+      if (isRight) {
+        window.statusTabRight = newTab;
+        window.updateSidebarRight();
+        } else {
+          window.statusTab = newTab;
+          window.updateSidebar();
+    }
   };
 
   window.onDisplayContent = function() {
